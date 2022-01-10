19e099f228e1f0e5941b5060ab449e432372d257
BrennanKnopf <brennanknopf@gmail.com>
2022-01-10 13:36:16 -0600

    Fixed choose_opponent method to handle user input error.

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 from human import Human
 from artificial_intelligence import Artificial_Intelligence
-
+import time
 
 
 class Game:
@@ -14,16 +14,22 @@ class Game:
     def run_game(self):
         self.display_greeting()
         self.display_rules()
-        self.choose_multiplayer()
+        self.choose_opponent()
         self.display_winner()
 
     def display_greeting(self):
         print('Welcome to Dueling Gestures')
+        time.sleep(1)
         print('Do you need to settle a score. Are you confident you can take on skynet?')
+        time.sleep(1)
         print('This is the place for you.')
+        time.sleep(1)
         print('The game is Rock, Paper, Scissors, Lizard, Spock.')
+        time.sleep(1)
         print('The game is a best of 3')
+        time.sleep(1)
         print('The rules work like this.')
+        time.sleep(1)
         
 
 
@@ -42,26 +48,39 @@ class Game:
         Paper disproves Spock 
         Spock vaporizes Rock
         ''')
+        time.sleep(3)
 
     
-    def choose_multiplayer(self):
-        self.player_opponent = int(input('Would you like to play sing player or multiplayer:Press 1 for single player game. Press 2 for multiplayer game.'))
-        if self.player_opponent == 1:
-            self.initiate_one_player_game()
-        elif self.player_opponent == 2:
-            self.initiate_two_player_game()
+    def choose_opponent(self):
+        self.player_opponent = int(input('Would you like to play sing player or multiplayer: Press 1 for single player game. Press 2 for multiplayer game.'))
+        choose_game = False
+        while choose_game == False:
+            if self.player_opponent != 1 and self.player_opponent != 2:
+               self.player_opponent = int(input('Not a valid choice. Please select again. Pease select 1 or 2'))
+            elif self.player_opponent == 1:
+                self.initiate_one_player_game()
+                choose_game = True
+            elif self.player_opponent == 2:
+                self.initiate_two_player_game()
+                choose_game = True
+            
+                  
     
     
     def initiate_one_player_game(self):
         self.player_one.choose_name()
+        print(f'{self.player_one.name} goes first.')
+        time.sleep(1)
         self.games_played = 0
         self.opponent = self.player_three
         while self.player_three.score < 2 and self.player_one.score < 2: #Can't get the game to step into this while loop???
             self.player_one_choice = self.player_one.choose_gesture()
             print(f'{self.player_one.name} chose {self.player_one.gesture_list[self.player_one_choice]}')
+            time.sleep(1)
             #print(self.player_one.choose_gesture())
             self.opponent_choice = self.player_three.choose_gesture()
             print(f'{self.opponent.name} chose {self.opponent.gesture_list[self.opponent_choice]}')
+            time.sleep(1)
             self.game_rules_method()
             #print(self.player_three.choose_gesture())
             self.games_played += 1
@@ -77,6 +96,7 @@ class Game:
         self.player_two.choose_name()
         self.games_played = 0
         self.opponent = self.player_two
+        print(f'{self.player_one.name} goes first.')
         while self.opponent.score < 2 and self.player_one.score < 2: #Can't get the game to step into this while loop???
             self.player_one_choice = self.player_one.choose_gesture()
             print(f'{self.player_one.name} chose {self.player_one.gesture_list[self.player_one_choice]}')
@@ -113,7 +133,7 @@ class Game:
             print(f'{self.player_one.name} wins with {self.player_one.gesture_list[4]} !')
             self.player_one.score = self.player_one.score + 1 
         else:
-            print('Player two wins!')
+            print(f'Player two wins with {self.opponent_choice}!')
             self.opponent.score = self.opponent.score + 1
     def round(self):
         pass
